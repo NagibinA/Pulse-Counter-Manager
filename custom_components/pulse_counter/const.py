8dd7105c6bbd680bf8008933100d696b7f29757a
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-06-16 07:11:02 +0300

    Update const.py

--- custom_components/pulse_counter/const.py
+++ custom_components/pulse_counter/const.py
@@ -1,7 +1,7 @@
 """Константы для интеграции Pulse Counter Manager."""
 
 DOMAIN = "pulse_counter"
-VERSION = "1.5.0"
+VERSION = "1.5.1"
 
 # Ссылка на официальные тарифы
 TARIFF_INFO_URL = "https://www.elec.ru/library/rd/tarify-elektroenergiya-2026/st-peterburg.html"
